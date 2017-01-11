51ae85fd6dacf5035463112a1f441685373f222b
njarigsma <nicky.jarigsma@gmail.com>
2017-01-11 16:04:44 +0100

    Welcome message

---
+++ pizza-delivery.rb
@@ -0,0 +1,28 @@
+#delivery - continous loop to help the customer
+
+done = false
+
+# welcome message
+puts "Hello, welcome to our pizzeria!"
+
+# this will run until done is set to true
+while not done
+  puts "What can I do for you?"
+  puts "1: order a pizza"
+  puts "2: nothing"
+  puts "-----------------"
+# get input
+  choice = gets.chomp.to_i
+  case choice
+  when 1
+    puts "Let me show you the menu"
+  when 2
+    done = true
+  else
+    puts "I don't understand"
+  end
+  if done
+    puts "Goodbye!"
+    puts "pizza image"
+  end
+end
